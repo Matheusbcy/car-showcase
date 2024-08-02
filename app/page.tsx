1d5148f46f581bb372511da9dbd2316e43af4bee
Matheus Freitas <matheusbcy@gmail.com>
2024-08-02 14:32:39 -0300

    searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import { Hero } from "@/components";
+import { Hero, SearchBar, CustomFilter } from "@/components"
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-        <Hero />
+      <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__textcontainer">
+          <h1 className="text-4xl font-extrabold">Catálogos de Carros</h1>
+          <p>Explore os carros que você pode gostar.</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="Fuel" />
+            <CustomFilter title="Year" />
+
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
